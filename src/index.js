d8086f506ae24065d230a4152773edd5f393e767
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-12 15:43:16 -0400

    add form with location label, input, and submit button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+const locationInputRef = document.querySelector("#location");
+const weatherDivRef = document.querySelector("#weather");
+
 async function getWeather(location) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=DCTFXU2LCMS8HUPLSW9LD42MS`);
-    return response.json();
+    const weatherData = await response.json();
+    return weatherData;
 }
+
+async function returnRelevantData(weatherData){
+    return {
+        temp: weatherData.currentConditions.temp,
+        feelsLike: weatherData.currentConditions.feelsLike,
+        humidity: weatherData.currentConditions.humidity,
+    };
+}
+
+
+
+
 
 getWeather("Sylvania");
