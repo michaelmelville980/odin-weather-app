92a86b2d5f34c55f7dacd2a197a8313f0c91a813
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-12 16:00:35 -0400

    adds relevant functions to get weather data based on location, extract relevant information, and update DOM all in response to form submission

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
-const locationInputRef = document.querySelector("#location");
-const weatherDivRef = document.querySelector("#weather");
+const locationInputRef = document.querySelector('#location');
+const weatherDivRef = document.querySelector('#weather');
+const formRef = document.querySelector('#form');
 
 async function getWeather(location) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=DCTFXU2LCMS8HUPLSW9LD42MS`);
-    const weatherData = await response.json();
-    return weatherData;
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=DCTFXU2LCMS8HUPLSW9LD42MS`
+  );
+  const weatherData = await response.json();
+  return weatherData;
 }
 
-async function returnRelevantData(weatherData){
-    return {
-        temp: weatherData.currentConditions.temp,
-        feelsLike: weatherData.currentConditions.feelsLike,
-        humidity: weatherData.currentConditions.humidity,
-    };
+function returnRelevantData(weatherData) {
+  return {
+    temp: weatherData.currentConditions.temp,
+    feelsLike: weatherData.currentConditions.feelslike,
+    humidity: weatherData.currentConditions.humidity,
+  };
 }
 
+async function displayWeatherData(relevantWeatherData) {
+  weatherDivRef.innerHTML = '';
 
+  const tempChild = document.createElement('p');
+  tempChild.textContent = 'Current Temperature: ' + relevantWeatherData.temp;
 
+  const feelsLikeChild = document.createElement('p');
+  feelsLikeChild.textContent =
+    'Feels Like Temperature: ' + relevantWeatherData.feelsLike;
 
+  const humidityChild = document.createElement('p');
+  humidityChild.textContent = 'Humidiy: ' + relevantWeatherData.humidity;
 
-getWeather("Sylvania");
+  weatherDivRef.append(tempChild, feelsLikeChild, humidityChild);
+}
+
+async function handleEverything(location) {
+  const weatherData = await getWeather(location);
+  const relevantData = returnRelevantData(weatherData);
+  displayWeatherData(relevantData);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  handleEverything(locationInputRef.value);
+});
